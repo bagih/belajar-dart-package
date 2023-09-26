7185758c8c21e010039e1b204599a1b492296d59
Bagi Hartawan <hartawanbagie@gmail.com>
2023-09-27 00:46:09 +0800

    initial commit

---
+++ example/helllo.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_package/hello.dart';
+
+void main() {
+  print(sayHello('bagih'));
+
+  Product product = Product();
+  Customer customer = Customer();
+  Category category = Category();
+}
